54c9aa415d1a5680dfc194122ec658156c8475c1
darylkdps <darylkdps-github@yahoo.com>
2022-10-19 09:16:52 +0800

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -2,14 +2,19 @@ import streamlit as st
 from pathlib import Path
 
 def main():
-    st.title('Home Page - Test of Streamlit Functionality')
+    st.title('Home Page - Transcription / Translation')
 
     if 'D:' in str(Path.cwd()) or 'C:' in str(Path.cwd()):
         st.markdown('_No token_')
     else:
-        st.markdown('_' + st.secrets['test_token'] + '_')
+        st.markdown('_' + st.secrets['token'] + '_')
 
 if __name__ == '__main__':
-    st.set_page_config(page_title="Daryl Test", page_icon="random", layout="wide", initial_sidebar_state="auto")
+    st.set_page_config(
+        page_title='Transcription / Translation',
+        page_icon='💬',
+        layout='wide',
+        initial_sidebar_state='auto')
+    
     main()
 
