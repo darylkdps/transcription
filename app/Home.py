f1939fcf9f0777cd6cc345abf91331801fe38159
darylkdps <darylkdps-github@yahoo.com>
2022-10-19 10:51:32 +0800

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -157,8 +157,8 @@ if file is not None and transcript_text != '':
 
     # Display preview of transcript
     preview_message = f'Previewing first {preview_length} segments of transcript.'
-    st.write(preview_message)
-    st.write(transcript_text_preview)
+    st.text(preview_message)
+    st.text(transcript_text_preview)
     
     # Display a file download button to download completed transcript
     st.download_button(
